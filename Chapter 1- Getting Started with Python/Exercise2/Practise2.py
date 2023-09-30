b267d998d20f76a538bc064a2a0cd67d580dbfd7
4bdulllahh <abdullahkamran823@gmail.com>
2023-09-30 20:08:47 +0400

    Update Practise2.py

--- Chapter 1- Getting Started with Python/Exercise2/Practise2.py
+++ Chapter 1- Getting Started with Python/Exercise2/Practise2.py
@@ -1,6 +1,6 @@
-import sys ##Imports "Sys" Module in py Script 
+import sys #Imports "Sys" Module in py Script 
 
-print("Python version") ##Prints python Version 
-print (sys.version) ##Prints Version of Python Currently Being used 
-print("Version info.") ##Prints version Info 
-print (sys.version_info) ##Print Output in a Tuple Containing 5 Components(major, minor, micro, releaselevel, and serial) 
+print("Python version") #Prints python Version 
+print (sys.version) #Prints Version of Python Currently Being used 
+print("Version info.") #Prints version Info 
+print (sys.version_info) #Prints Output in a Tuple Containing 5 Components(major, minor, micro, release level, and serial) 
