6d5894ed19383dbd2ac0230d7b342834e94aabd1
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-01 19:18:46 +0400

    Practise2
    write py to print time and date Practise2

---
+++ Chapter 1- Getting Started with Python/Exercise3/Practise2.py
@@ -0,0 +1,6 @@
+import datetime # Get the current date and time
+
+current_datetime = datetime.datetime.now() # Display the current date and time
+
+print("Current Date and Time:")
+print(current_datetime)
