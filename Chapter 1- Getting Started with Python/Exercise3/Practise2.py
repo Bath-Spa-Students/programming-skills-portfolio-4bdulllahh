f09948f948406824472c6b810d8b2eb29c97b5c3
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-02 09:52:56 +0400

    Update Practise2.py

--- Chapter 1- Getting Started with Python/Exercise3/Practise2.py
+++ Chapter 1- Getting Started with Python/Exercise3/Practise2.py
@@ -2,5 +2,6 @@ import datetime # Get the current date and time
 
 current_datetime = datetime.datetime.now() # Display the current date and time
 
-print("Current Date and Time:")
-print(current_datetime)
+print("Current Date and Time:") #This line is a print statement that displays the text "Current date and time : " on the console.
+#It's just a label to indicate what will be printed next.
+print(current_datetime) #This line prints the formatted date and time to the console.
