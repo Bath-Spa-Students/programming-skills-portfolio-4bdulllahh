d952170b4924204d80716a422529631b05fa7f82
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-02 09:31:59 +0400

    Update Practise1.py

--- Chapter 1- Getting Started with Python/Exercise3/Practise1.py
+++ Chapter 1- Getting Started with Python/Exercise3/Practise1.py
@@ -1,4 +1,6 @@
-import datetime
-now = datetime.datetime.now()
-print ("Current date and time : ")
-print (now.strftime("%Y-%m-%d %H:%M:%S"))
+import datetime # This line imports the datetime module, which is a built-in module in Python used for working with dates and times.
+
+now = datetime.datetime.now() #In this line, the datetime.datetime.now() function is called to retrieve the current date and time.
+print ("Current date and time : ") #This line is a print statement that displays the text "Current date and time : " on the console.
+#It's just a label to indicate what will be printed next.
+print (now.strftime("%Y-%m-%d %H:%M:%S")) #This line prints the formatted date and time to the console. 
