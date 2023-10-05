c1bf43b9d3ed368dc6bf2c4a02b92c558b388143
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-05 13:17:02 +0400

    Update Practise2.py

--- Chapter 1- Getting Started with Python/Practise/Practise2.py
+++ Chapter 1- Getting Started with Python/Practise/Practise2.py
@@ -1,4 +1,4 @@
 #Create a Program to Output "Hi" 50 Times, Without any seperator 
 for _ in range(50): #This loop is used to repeat a set of instructions a specific number of times, in this case, 50 times.
     print("Hi", end="") #"print" function used to print given word
-                        #"end=""" is default set to /n which means newline but here we add "" which means nothing will be printed after "Hi"
+                        #" end="" " is default set to \n which means newline but here we add "" which means nothing will be printed after "Hi"
