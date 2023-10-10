e1d07b334aa57bc428f626275d9ef912bfc6ca25
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-10 20:09:24 +0400

    Practise 4 and 5

--- Chapter 1- Getting Started with Python/Practise/Practise4.py
+++ Chapter 1- Getting Started with Python/Practise/Practise4.py
@@ -1,1 +1,8 @@
 #Write four strings in diffrent variables and print the output as one string(String Concatenation)
+string1 = "I "
+string2 = "Study "
+string3 = "Creative " 
+string4 = "Computing. " # Define three strings in different variables
+
+output_string = string1 + string2 + string3 + string4
+print(output_string) # Concatenate the strings and print them as one string
--- Chapter 1- Getting Started with Python/Practise/Practise5.py
+++ Chapter 1- Getting Started with Python/Practise/Practise5.py
@@ -1,1 +1,10 @@
 #Compute the Area of a Triangle 
+# Ask the user for the length of the base and the height
+base = float(input("Please enter the length of the base of the triangle: "))
+height = float(input("Please enter the height of the triangle: "))
+
+# Calculate the area of the triangle
+area = (base * height) / 2
+
+# Print the calculated area
+print("The area of the triangle is:", area)
