7c8cdf8f5ae44c5088a139f6d7e06d4afde2bfae
Abdullah <abdullahkamran823@gmail.com>
2023-10-11 23:08:19 +0400

    Update Practise3.py

--- Chapter 1- Getting Started with Python/Exercise3/Practise3.py
+++ Chapter 1- Getting Started with Python/Exercise3/Practise3.py
@@ -1,7 +1,8 @@
-import datetime
+import datetime #Imports date and time from module 
 
-now = datetime.datetime.now()
+now = datetime.datetime.now() #'now' is the name give to the function 'datetime.datetime.now()' which obtains the current date and time to the fraction of the second
 
-formatted_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
+formatted_datetime = now.strftime("%Y-%m-%d %H:%M:%S") #'strftime' funtion is used to specify the format we want to show the date and time in 
+#here we choosed the foramt ---> YYYY-MM-DD HH:MM:SS 
 
-print("Current Date and Time:" ,formatted_datetime)
+print("Current Date and Time:" ,formatted_datetime) # We add 'Cuurent Date and Time' in the beginning to show what is being printed then the formatted date and time  
