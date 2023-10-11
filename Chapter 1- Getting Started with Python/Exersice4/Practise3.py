ab3a2877ec60fc61af99ab007b6bd9e9487b3c8e
Abdullah <abdullahkamran823@gmail.com>
2023-10-11 23:31:13 +0400

    Pracise3

---
+++ Chapter 1- Getting Started with Python/Exersice4/Practise3.py
@@ -0,0 +1,10 @@
+# Define three strings in different variables
+string1 = "Hello, "
+string2 = "world! "
+string3 = "How are you?"
+
+# Use string formatting to combine the strings
+combined_string = "{}{}{}".format(string1, string2, string3)
+
+# Print the combined string
+print(combined_string)
