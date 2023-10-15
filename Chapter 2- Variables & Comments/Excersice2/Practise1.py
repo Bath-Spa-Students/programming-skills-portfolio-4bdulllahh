2c025e53e17f724cfef352d6809af97414e6a61e
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-15 11:17:01 +0400

    Excercise 2

--- Chapter 2- Variables & Comments/Excersice2/Practise1.py
+++ Chapter 2- Variables & Comments/Excersice2/Practise1.py
@@ -2,7 +2,7 @@ def print_famous_quote(quote, author):
     formatted_quote = f'{author} once said, "{quote}"'
     print(formatted_quote)
 
-# Replace these with your desired quote and author
+# Replace these with your quote and author
 quote = "The supreme art of war is to subdue the enemy without fighting.."
 author = "Sun Tzu"
 
