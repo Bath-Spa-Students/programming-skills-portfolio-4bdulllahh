9cb18db053b2c22c5e14fa1fab732f1bb329dc40
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-15 14:03:04 +0400

    Update Practise1.py

--- Chapter 2- Variables & Comments/Excersice4/Practise1.py
+++ Chapter 2- Variables & Comments/Excersice4/Practise1.py
@@ -1,4 +1,4 @@
 favorite_number = 8  # Replace 8 with your favorite number
 
-message = "My favorite number is {favorite_number}."
+message = "My favorite number is (favorite_number)."
 print(message)
