7a07efafa1a7e1917646732e7794f9d573cd220c
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-15 19:31:40 +0400

    ex5 practise 2

---
+++ Chapter 2- Variables & Comments/Excersice5/Practise2.py
@@ -0,0 +1,7 @@
+Budget = 50 
+USB_cost = 6
+amount_buyable = Budget // USB_cost
+remaining_change = Budget % amount_buyable
+print("The girl can buy", amount_buyable, "USB sticks.")
+print("The girl will have £", remaining_change, "left.")
+
