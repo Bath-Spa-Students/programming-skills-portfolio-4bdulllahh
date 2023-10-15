f9564ce9e5981c4abef363f69286c76878452356
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-15 19:23:55 +0400

    Ex3 practise 3

---
+++ Chapter 2- Variables & Comments/Excersice3/Practise3.py
@@ -0,0 +1,6 @@
+name = "\t\n   Ahmed Hussain   \t\n"
+
+print(f"Original Name: '{name}'")
+print(f"Name with Leading Whitespace Removed: '{name.lstrip()}'")
+print(f"Name with Trailing Whitespace Removed: '{name.rstrip()}'")
+print(f"Name with Both Leading and Trailing Whitespace Removed: {name.strip()}")
