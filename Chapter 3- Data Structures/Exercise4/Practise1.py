d2bf4b156241644b1faf73cbbf2735a06648db32
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-22 16:41:50 +0400

    Exercise4

---
+++ Chapter 3- Data Structures/Exercise4/Practise1.py
@@ -0,0 +1,7 @@
+guest = ("Robin Williams", "Keanu Reeves", "Rowan Antikson")
+guest1 = guest[0]
+print(f"Dear", guest1,", I Invite you to my Grand Ballroom party")
+guest2 = guest[1]
+print(f"Dear", guest2,", I Invite you to my Grand Ballroom party")
+guest3 = guest[2]
+print(f"Dear", guest3,", I Invite you to my Grand Ballroom party")
