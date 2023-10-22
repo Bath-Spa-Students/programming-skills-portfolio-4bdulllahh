04585fa6ee213e5a8fbd93a9e03963d8bef6b3cc
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-22 21:01:31 +0400

    Exercise7

---
+++ Chapter 3- Data Structures/Exercise7/Practise1.py
@@ -0,0 +1,36 @@
+# Create a list of places to visit (not in alphabetical order)
+list = ["Tokyo", "Paris", "Saudia Arabia", "Monacco", "Rome"]
+
+#original order
+x = list 
+print(x)
+
+#alphabetical order
+y = sorted(list)
+print(y)
+
+# Check that the original list is still in its original order
+print(x)
+
+#reverse alphabetical order
+z = sorted(list, reverse=True)
+print(z)
+
+# Check original list is still in original order
+print("Original list:", list)
+
+# Use reverse() to change the order of the list
+list.reverse()
+print(list)
+
+# Use reverse() again to change the order back to the original
+list.reverse()
+print(list)
+
+# Use sort() to change the list to alphabetical order
+list.sort()
+print(list)
+
+# Use sort() to change the list to reverse alphabetical order
+list.sort(reverse=True)
+print(list)
---
+++ Chapter 3- Data Structures/Exercise7/Practise2.py
@@ -0,0 +1,33 @@
+# Create a list of places to visit (not in alphabetical order)
+places_to_visit = ["Tokyo", "Paris", "Saudia Arabia", "Monacco", "Rome"]
+
+#original order
+print("Original list:", places_to_visit)
+
+#alphabetical order
+print("Sorted list in alphabetical order:", sorted(places_to_visit))
+
+# Check that the original list is still in its original order
+print("Original list (still in original order):", places_to_visit)
+
+#reverse alphabetical order
+print("Sorted list in reverse alphabetical order:", sorted(places_to_visit, reverse=True))
+
+# Check that the original list is still in its original order
+print("Original list (still in original order):", places_to_visit)
+
+# Use reverse() to change the order of the list
+places_to_visit.reverse()
+print("Reversed list:", places_to_visit)
+
+# Use reverse() again to change the order back to the original
+places_to_visit.reverse()
+print("Reversed list (back to original order):", places_to_visit)
+
+# Use sort() to change the list to alphabetical order
+places_to_visit.sort()
+print("List in alphabetical order:", places_to_visit)
+
+# Use sort() to change the list to reverse alphabetical order
+places_to_visit.sort(reverse=True)
+print("List in reverse alphabetical order:", places_to_visit)
