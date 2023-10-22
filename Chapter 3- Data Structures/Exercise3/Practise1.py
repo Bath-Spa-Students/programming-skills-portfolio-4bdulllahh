d71f67348f83b33a3cf39e027a9ef906f723d7dc
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-22 16:26:26 +0400

    Exercise3

---
+++ Chapter 3- Data Structures/Exercise3/Practise1.py
@@ -0,0 +1,4 @@
+transportation = ("Kawasaki Ninja", "R34 GTR", "Ford GT")
+print("I Would love to ride a", transportation[0], "One day")
+print("My Dream car is the", transportation[1])
+print("The car I would like to race in is the", transportation[2])
