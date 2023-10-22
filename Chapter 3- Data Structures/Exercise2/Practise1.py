fa5d5bed8c0d4d0187623b0c1a6b4fe1d0b04bb4
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-22 14:28:59 +0400

    Exercise2

---
+++ Chapter 3- Data Structures/Exercise2/Practise1.py
@@ -0,0 +1,6 @@
+names = ["Abdullah", "Ahmed", "Mohammed", "Omar", "Ali"]
+print("My message to",names[0])
+print("My message to",names[1])
+print("My message to",names[2])
+print("My message to",names[3])
+print("My message to",names[4])
---
+++ Chapter 3- Data Structures/Exercise2/Practise2.py
@@ -0,0 +1,6 @@
+data = ("Adil" , "Faisal" , "Abubakar" , "Shakin" , "Obaid")
+print("The man",data[0], "Should rule the world :)")
+print("The man",data[1], "Should rule the world :)")
+print("The man",data[2], "Should rule the world :)")
+print("The man",data[3], "Should rule the world :)")
+print("The man",data[4], "Should rule the world :)")
