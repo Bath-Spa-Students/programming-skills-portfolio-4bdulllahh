caec0418c7a03d91a917fa60c9dab6be83e3850a
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-22 13:20:59 +0400

    Exercise1

---
+++ Chapter 3- Data Structures/Exercise1/Practise1.py
@@ -0,0 +1,6 @@
+# Create a list of names
+names = ["Abdullah", "Ahmed", "Mohammed", "Omar", "Ali"]
+
+# Print each name one at a time
+for name in names:
+    print(name)
---
+++ Chapter 3- Data Structures/Exercise1/Practise2.py
@@ -0,0 +1,12 @@
+data = ("Adil" , "Faisal" , "Abubakar" , "Shakin" , "Obaid")
+
+print (data[0])
+
+print (data[1])
+
+print (data[2])
+
+print (data[3])
+
+print (data[4])
+
