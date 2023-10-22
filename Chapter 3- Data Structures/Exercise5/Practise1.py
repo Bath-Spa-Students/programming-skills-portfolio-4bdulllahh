1c080a43ebf79d986f1fbecab2677c9efc7b2be6
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-22 17:56:52 +0400

    Exercise5

---
+++ Chapter 3- Data Structures/Exercise5/Practise1.py
@@ -0,0 +1,13 @@
+# Initial list of guests
+guests = ["Robin Williams", "Keanu Reeves", "Rowan Atkinson"]
+
+# Print the guest who can't make it
+guest_cant_make_it = "Robin Williams"
+print(f"Unfortunately, {guest_cant_make_it} can't make it to the dinner.")
+
+# Replace the guest who can't make it with a new person
+new_guest = "Audrey Hepburn"
+guests[0] = new_guest
+
+# Generate new invitations
+print(f"Dear", guests[0], " I would like to invite you to dinner.")
