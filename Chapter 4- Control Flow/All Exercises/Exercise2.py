999e8f91998fd4b7fe2363175fbd7494c1ea7707
Abdullah <abdullahkamran823@gmail.com>
2023-10-28 14:42:14 +0400

    Exercise 2

---
+++ Chapter 4- Control Flow/All Exercises/Exercise2.py
@@ -0,0 +1,8 @@
+print ("alien colors: red, yellow, green")
+
+alien_color = input("What Colour was the Alien?: ")
+
+if alien_color == "green":
+    print ("You earned 5 points.")
+else: 
+    print ("You earned 10 points.")
