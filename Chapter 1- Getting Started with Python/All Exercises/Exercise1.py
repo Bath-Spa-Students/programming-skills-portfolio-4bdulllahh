70c716a821fb5d5370342c131a48e45863083d20
4bdulllahh <abdullahkamran823@gmail.com>
2023-11-05 21:52:19 +0400

    Exercise 2

--- Chapter 1- Getting Started with Python/All Exercises/Exercise1.py
+++ Chapter 1- Getting Started with Python/All Exercises/Exercise1.py
@@ -4,3 +4,5 @@ print("""Twinkle, twinkle, little star,
 		Like a diamond in the sky. 
 Twinkle, twinkle, little star, 
 	How I wonder what you are""")
+
+	
--- Chapter 1- Getting Started with Python/All Exercises/Exercise2.py
+++ Chapter 1- Getting Started with Python/All Exercises/Exercise2.py
@@ -3,4 +3,6 @@ import sys #Imports "Sys" Module in py Script
 print("Python version") #Prints python Version 
 print (sys.version) #Prints Version of Python Currently Being used 
 print("Version info.") #Prints version Info 
-print (sys.version_info) #Prints Output in a Tuple Containing 5 Components(major, minor, micro, release level, and serial) 
+print (sys.version_info) #Prints Output in a Tuple Containing 5 Components
+
+
