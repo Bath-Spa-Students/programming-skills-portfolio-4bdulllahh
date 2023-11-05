5aa6e99d043d91f939c043ee4d49ac7c7ddb9326
4bdulllahh <abdullahkamran823@gmail.com>
2023-11-05 21:59:52 +0400

    Exercise 5

--- Chapter 1- Getting Started with Python/All Exercises/Exercise5.py
+++ Chapter 1- Getting Started with Python/All Exercises/Exercise5.py
@@ -1,14 +1,10 @@
 # Import the math module to access the value of pi
 import math
-
 # Ask the user for the radius of the circle
 radius = float(input("Please enter the radius of the circle: "))
-
 # Calculate the area of the circle
 area = math.pi * radius**2
-
 # Round the area to 2 decimal places
 rounded_area = round(area, 2)
-
 # Print the rounded area
 print("The area of the circle with radius", radius, "is:", rounded_area)
