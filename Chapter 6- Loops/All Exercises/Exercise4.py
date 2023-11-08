30bee6bee54561d321493f71b2aa1d50d4fe1763
Abdullah <abdullahkamran823@gmail.com>
2023-11-08 22:02:30 +0400

    Exercise 4

--- Chapter 6- Loops/All Exercises/Exercise4.py
+++ Chapter 6- Loops/All Exercises/Exercise4.py
@@ -5,9 +5,10 @@ sandwich_orders = ["Cheese Sandwich", "Falefel Sandwich", "Chicken Sandwich", "S
 finished_sandwiches = []
 
 # Loop through the sandwich orders
+#while loop runs until there are sandwiches in the sandwich_order list 
 while sandwich_orders:
     current_sandwich = sandwich_orders.pop(0) #pop(0) removes the item from the list and moves or assigns it to 'current_sandwich'
-    print(f"I made your {current_sandwich}.")
+    print(f"I made your {current_sandwich}.") #prints sandwich removed
     finished_sandwiches.append(current_sandwich)
 
 # Displaying the finished sandwiches
