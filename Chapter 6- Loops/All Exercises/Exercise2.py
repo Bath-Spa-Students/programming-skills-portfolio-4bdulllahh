6b1bef31f72928da81b45d0f9221d62a6e933799
Abdullah <abdullahkamran823@gmail.com>
2023-11-08 21:44:34 +0400

    Exercise 2

---
+++ Chapter 6- Loops/All Exercises/Exercise2.py
@@ -0,0 +1,16 @@
+#while command to start infinite loop
+while True:
+    age = input("Please enter your age (or 'leave' to exit): ")
+#if user types 'leave' the loop will end
+    if age.lower() == 'leave':
+        print("Thank you for using the ticket service. See yaaa!")
+        break
+
+    age = int(age)
+#if and elif statments for diffrent ages, will print diffrent messages for diffrent ages
+    if age < 3:
+        print("Your Ticket is free.")
+    elif 3 <= age <= 12:
+        print("Your Ticket will cost $10.")
+    else:
+        print("Your Ticket will cost $15.")
