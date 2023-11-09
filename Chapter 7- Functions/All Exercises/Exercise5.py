fbbab950026d63ec05a4864c5b46334a1fb517ad
Abdullah <abdullahkamran823@gmail.com>
2023-11-09 19:11:36 +0400

    Exercise 5

---
+++ Chapter 7- Functions/All Exercises/Exercise5.py
@@ -0,0 +1,15 @@
+
+def describe_city(city,country):
+  print(city,"is located in",country)
+#city and country 1 
+a = str(input("Enter city: "))
+b = str(input("Enter the country it is located in: "))  
+describe_city(a,b)
+#city and country 2
+c = str(input("Enter city: "))
+d = str(input("Enter the country it is located in: "))  
+#city and country 3
+describe_city(c,d)
+e = str(input("Enter city: "))
+f = str(input("Enter the country it is located in: "))  
+describe_city(e,f)
