36abb922aa8be8392a3bfbac563f674bc64ab096
Abdullah <abdullahkamran823@gmail.com>
2023-11-09 18:53:21 +0400

    Exercise 3

--- Chapter 7- Functions/All Exercises/Exercise3.py
+++ Chapter 7- Functions/All Exercises/Exercise3.py
@@ -1,9 +1,11 @@
 #def function to define function 
 def make_shirt(size,message):
   print("Your shirt size is ",size," and it says ",message,)
+#incase user doesnt write anything  
 a = "S" 
-b = "hello" 
+b = "Heyyaa" 
 make_shirt(a,b)  
+#ask user thier shirt size and what they want on thier shirt
 shirtsize = str(input("Enter your shirt size (S,M,L,XL,XXL): "))##S,M,L
 txt = str(input("Enter the message you want to print on the shirt: "))
 make_shirt(shirtsize,txt)
