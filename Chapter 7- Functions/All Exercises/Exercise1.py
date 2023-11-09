fb3072cf52d555c5f71bf07a9a26beb834d75874
Abdullah <abdullahkamran823@gmail.com>
2023-11-09 18:37:38 +0400

    Exercise 1

--- Chapter 7- Functions/All Exercises/Exercise1.py
+++ Chapter 7- Functions/All Exercises/Exercise1.py
@@ -1,5 +1,6 @@
 #use def command to define a function
 def display_message():
-    print("In this chapter, we will be learning about arrays, dictionaries, and loops.")
+    print("In this chapter, we will be learning about Functions.")
 
 display_message()
+#displays the message
