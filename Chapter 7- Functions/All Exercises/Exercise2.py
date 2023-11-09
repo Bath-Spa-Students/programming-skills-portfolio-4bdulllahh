78467089809de33598d79d1d2d13d8528186bceb
Abdullah <abdullahkamran823@gmail.com>
2023-11-09 18:45:09 +0400

    Exercise 2

--- Chapter 7- Functions/All Exercises/Exercise2.py
+++ Chapter 7- Functions/All Exercises/Exercise2.py
@@ -1,5 +1,5 @@
 #def command to define function
-#instead of giving book value we ask the user
+#instead of giving book value we ask the user to write thier favorite book
 def favourite_book(book):
   print(book," is one of my favourites to read!")
 #asking the user thier favorite book
