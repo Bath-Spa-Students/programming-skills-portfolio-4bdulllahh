5e28ae42b0193a9a8dbffbdd8957da1cdbe5c2e0
Abdullah <abdullahkamran823@gmail.com>
2023-11-10 16:38:41 +0400

    Practise 4

---
+++ Chapter 3- Data Structures/Practise/Practise4.py
@@ -0,0 +1,10 @@
+# Assuming 'List1
+#' is a list with 100 elements
+List1 = [i for i in range(1, 101)]  # Example list with 100 elements
+# Creating an empty list 'numbers2'
+numbers2 = []
+# Copying values from 'List1
+#' to 'numbers2'
+numbers2 = List1.copy()
+# Print 'numbers2' to verify the copy
+print("numbers2:", numbers2)
