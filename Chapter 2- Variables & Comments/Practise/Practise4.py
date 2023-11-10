3feffc9dcb48729c2dc2243299d4c34ce1b73cb8
Abdullah <abdullahkamran823@gmail.com>
2023-11-10 16:15:08 +0400

    Practise 4

---
+++ Chapter 2- Variables & Comments/Practise/Practise4.py
@@ -0,0 +1,7 @@
+# Store an integer and a string in variables x and y
+x = 42  # Example integer value
+y = "Hello Cruel World"  # Example string value
+
+# Print the types of variables x and y
+print(f"Type of variable x: {type(x)}")
+print(f"Type of variable y: {type(y)}")
