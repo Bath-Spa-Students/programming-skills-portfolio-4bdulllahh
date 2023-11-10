9041a80c070dd7688f3b26d322845afd946cd13e
Abdullah <abdullahkamran823@gmail.com>
2023-11-10 16:16:14 +0400

    Practise 5

---
+++ Chapter 2- Variables & Comments/Practise/Practise5.py
@@ -0,0 +1,10 @@
+# Store fruit names in a list
+fruits = ["Apple", "Banana", "Orange"]
+
+# Unpack the list into variables x, y, and z
+x, y, z = fruits
+
+# Print the values of each variable
+print("Value of x:", x)
+print("Value of y:", y)
+print("Value of z:", z)
