cd660ee81e0c5fdf2a9c9246dde6ac181ea6d51a
Abdullah <abdullahkamran823@gmail.com>
2023-11-12 13:17:01 +0400

    Exercise 1

--- Chapter 6- Loops/All Exercises/Exercise1.py
+++ Chapter 6- Loops/All Exercises/Exercise1.py
@@ -2,8 +2,9 @@ topping = ''  # Initializing the variable to enter the loop
 #The while loop continues as long as the condition topping.lower() != 'quit' is true.
 #we add .lower to ensure even if the use write 'quit' in caps the code recognises it 
 while topping.lower() != 'quit':
-    topping = input("Enter a topping to add to your pizza (or 'quit' to finish): ") #Lets the user to input a topping for the pizza or enter 'quit' to finish
+    #Lets the user to input a topping for the pizza or enter 'quit' to finish
 
+    topping = input("Enter a topping to add to your pizza (or 'quit' to finish): ") 
     if topping.lower() != 'quit':
         if topping.lower() == 'pineapple':
             print("You shouldn't do that but... ok, I'll add that to your pizza.")
