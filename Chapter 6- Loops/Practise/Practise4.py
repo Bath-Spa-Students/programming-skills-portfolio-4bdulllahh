eada776d3dc211e97c4726738467cab76ed2079a
Abdullah <abdullahkamran823@gmail.com>
2023-11-16 00:29:42 +0400

    Practise 4

---
+++ Chapter 6- Loops/Practise/Practise4.py
@@ -0,0 +1,11 @@
+# Using a for loop with a break statement
+for number in range(1, 11):
+    print("Current number:", number)
+    
+    # Check a condition to break the loop
+    if number == 5:
+        print("Condition met. Exiting the loop.")
+        break
+
+# This code will continue here after the loop is exited
+print("Loop finished.")
