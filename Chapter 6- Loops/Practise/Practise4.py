bf98196854f18c1b619fdc678a315387118c91fa
Abdullah <abdullahkamran823@gmail.com>
2023-11-16 00:33:30 +0400

    Practise 4

--- Chapter 6- Loops/Practise/Practise4.py
+++ Chapter 6- Loops/Practise/Practise4.py
@@ -5,6 +5,7 @@ for number in range(1, 11):
     # Check a condition to break the loop
     if number == 5:
         print("Condition met. Exiting the loop.")
+        #will break the loop
         break
 
 # This code will continue here after the loop is exited
