9ef1eb8412087a4a52e2d814f8f0f701d2065cec
Abdullah <abdullahkamran823@gmail.com>
2023-11-15 23:08:01 +0400

    Practise 1

--- Chapter 4- Control Flow/Practise/Practise1.py
+++ Chapter 4- Control Flow/Practise/Practise1.py
@@ -1,5 +1,15 @@
-temparture = float(input("Input temparture: "))
-if temparture < 40:
-    print("A little cold, isn't it?")
-else :
-    print("Nice Weather we're Having")
+# Assuming there is a variable named x
+x = 120  # You can replace this value with the desired value
+
+# Using an if statement to check if x is greater than 100
+if x > 100:
+    y = 20
+    z = 40
+    #diffrent messages for diffrent scenarios
+    print(f"x is greater than 100. The Value of y is changed into 20, and the value of z is assigned to 40.")
+else:
+    print(f"x is not greater than 100. The value of y and z does not change.")
+
+# Printing the values of y and z
+print("Value of y:", y)
+print("Value of z:", z)
