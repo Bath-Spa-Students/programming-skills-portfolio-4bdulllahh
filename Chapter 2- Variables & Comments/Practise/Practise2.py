75890eb2b0f2728cf51ce73e1d458b275a29e369
Abdullah <abdullahkamran823@gmail.com>
2023-11-15 22:56:41 +0400

    Practise 2

--- Chapter 2- Variables & Comments/Practise/Practise2.py
+++ Chapter 2- Variables & Comments/Practise/Practise2.py
@@ -1,19 +1,19 @@
 # Initialize variables
-num_courses = int(input("Enter the number of courses: "))  # Number of courses
+number_of_courses = int(input("Enter the number of courses: "))  # Number of courses
 total_marks = float(input("Enter the total marks: "))  # Total marks for all courses
 course_marks = []  # List to store marks for each course
 
 # Input: Marks for each course
-for i in range(num_courses):
+for i in range(number_of_courses):
     mark = float(input(f"Enter the marks for course {i + 1}: "))
     course_marks.append(mark)
 
 # Calculate average
-total_marks_obtained = sum(course_marks)
-average_marks = total_marks_obtained / num_courses
+total_marks_got = sum(course_marks)
+average_marks = total_marks_got / number_of_courses
 
 # Calculate percentage
-percentage = (total_marks_obtained / total_marks) * 100
+percentage = (total_marks_got / total_marks) * 100
 
 # Print the results
 print("\nResults:")
