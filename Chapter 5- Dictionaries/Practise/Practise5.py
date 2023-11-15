f2305e16fc50704f70e68da801efe6eb1c43c7ce
Abdullah <abdullahkamran823@gmail.com>
2023-11-16 00:20:52 +0400

    Practise 5

--- Chapter 5- Dictionaries/Practise/Practise5.py
+++ Chapter 5- Dictionaries/Practise/Practise5.py
@@ -1,6 +1,6 @@
 # Two dictionaries to merge
 dict1 = {"name": "Abdullah", "age": 18, "city": "Umm al Qwain"}
-dict2 = {"occupation": "student", "hobbies": ["reading", "coding"]}
+dict2 = {"occupation": "student", "hobbies": ["Cars", "coding"]}
 
 # Merging dictionaries using update() method
 merged_dict = dict1.copy()  # Create a copy of dict1 to avoid modifying it directly
