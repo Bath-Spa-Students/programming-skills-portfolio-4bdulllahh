eaac43557bc680cadec2b803de64aab326415199
Abdullah <abdullahkamran823@gmail.com>
2023-11-16 00:34:38 +0400

    Practise 4

--- Chapter 6- Loops/Practise/Practise4.py
+++ Chapter 6- Loops/Practise/Practise4.py
@@ -1,5 +1,6 @@
 # Using a for loop with a break statement
 for number in range(1, 11):
+    #will start printing from first number in range
     print("Current number:", number)
     
     # Check a condition to break the loop
