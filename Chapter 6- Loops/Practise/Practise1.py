8a39982f33f1ce89a240457503633eb0a4723f8a
Abdullah <abdullahkamran823@gmail.com>
2023-11-16 00:30:41 +0400

    Practise 1

--- Chapter 6- Loops/Practise/Practise1.py
+++ Chapter 6- Loops/Practise/Practise1.py
@@ -1,7 +1,9 @@
-# Initialize a variable with the starting number
+#variable with the starting number
 number = 2
 
 # Using a while loop to print even numbers from 2 to 10
 while number <= 10:
+    #prints the first varaible to begin with
     print(number)
+    #adds 2 each time
     number += 2  # Increment by 2 to get the next even number
