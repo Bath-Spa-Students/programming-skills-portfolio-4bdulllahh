d8860d0f87a2b96a8f42c2da77bd000eec74327d
Abdullah <abdullahkamran823@gmail.com>
2023-11-16 00:07:23 +0400

    Practise 2

---
+++ Chapter 5- Dictionaries/Practise/Practise2.py
@@ -0,0 +1,15 @@
+# Creating a dictionary
+myself_info = {
+    "name": "Your Name",
+    "age": 25,
+    "gender": "Female",
+    "occupation": "Software Developer",
+    "location": "City, Country",
+    "hobbies": ["reading", "coding", "traveling"],
+    "favorite_color": "Blue",
+}
+
+# Iterating through the keys and printing them
+print("Keys of the dictionary:")
+for key in myself_info.keys():
+    print(key)
