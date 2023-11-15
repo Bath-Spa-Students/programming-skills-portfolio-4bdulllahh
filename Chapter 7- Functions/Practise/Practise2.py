aa415bad23b317f40d86cae0ba64230d6bd3ffe8
Abdullah <abdullahkamran823@gmail.com>
2023-11-16 00:58:03 +0400

    Practise 2

--- Chapter 7- Functions/Practise/Practise2.py
+++ Chapter 7- Functions/Practise/Practise2.py
@@ -1,26 +1,27 @@
-def factorial(n):
+def calculate_factorial(number):
     """
-    Recursive function to calculate the factorial of a given positive integer.
+    Recursive function to determine the factorial of a given positive integer.
 
     Parameters:
-    - n: Positive integer
+    - number: Positive integer
 
     Returns:
-    Factorial of n
+    Factorial of the given number
     """
     # Base case: factorial of 0 or 1 is 1
-    if n == 0 or n == 1:
+    if number == 0 or number == 1:
         return 1
     else:
-        # Recursive case: n! = n * (n-1)!
-        return n * factorial(n - 1)
+        # Recursive case: factorial(n) = n * factorial(n-1)
+        return number * calculate_factorial(number - 1)
 
 # Example usage
 user_input = int(input("Enter a positive integer: "))
 
-# Check if the input is a positive integer
+# Verify if the input is a positive integer
 if user_input < 0:
     print("Please enter a positive integer.")
 else:
-    result_factorial = factorial(user_input)
+    result_factorial = calculate_factorial(user_input)
     print(f"The factorial of {user_input} is: {result_factorial}")
+
