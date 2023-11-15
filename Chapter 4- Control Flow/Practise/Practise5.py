a78ad9a51cec44c1bb15d30c7c1d27a634febfb4
Abdullah <abdullahkamran823@gmail.com>
2023-11-15 23:57:40 +0400

    Practise 5

--- Chapter 4- Control Flow/Practise/Practise5.py
+++ Chapter 4- Control Flow/Practise/Practise5.py
@@ -8,7 +8,7 @@ month = input("Enter the month (e.g., January, February, etc.): ").lower()  # Co
 if month in valid_months:
     # Using elif statements to determine the season based on the month
     if month in ["december", "january", "february"]:
-        season = "Winter"
+        season = "Winter" #sets season variable to winter
     elif month in ["march", "april", "may"]:
         season = "Spring"
     elif month in ["june", "july", "august"]:
