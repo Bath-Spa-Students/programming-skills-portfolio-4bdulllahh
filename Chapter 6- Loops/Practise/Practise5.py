fe557888a2a050779d9f115e5b37d5801feb4b99
Abdullah <abdullahkamran823@gmail.com>
2023-11-16 00:37:12 +0400

    Practise 5

---
+++ Chapter 6- Loops/Practise/Practise5.py
@@ -0,0 +1,20 @@
+# Initialize a variable to store the largest number
+largest_number = float('-inf')  # Start with a very small value
+
+while True:
+    # Take user input
+    user_input = input("Enter a number (enter '0' to exit): ")
+
+    # Check if the user wants to exit
+    if user_input == '0':
+        break
+
+    # Convert the user input to a float
+    number = float(user_input)
+
+    # Update the largest number if the current input is greater
+    if number > largest_number:
+        largest_number = number
+
+# Print the largest number when the loop exits
+print("The largest number entered is:", largest_number)
