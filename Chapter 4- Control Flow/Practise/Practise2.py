77b9ceaa6b3e47d873b59673467d01449ebac8b0
Abdullah <abdullahkamran823@gmail.com>
2023-11-15 23:12:00 +0400

    Practise 2

---
+++ Chapter 4- Control Flow/Practise/Practise2.py
@@ -0,0 +1,11 @@
+# Taking user input for-- a
+a = int(input("Enter a value for a: "))
+
+# Using an if-else statement to check if a is less than 10
+if a < 10:
+    b = 'O'
+else:
+    b = 99
+
+# Printing the value of b
+print("Value of b:", b)
