0328e7d670563dc2b512a554a4dbea7ada0ab37d
Abdullah <abdullahkamran823@gmail.com>
2023-11-16 00:28:14 +0400

    Practise 3

---
+++ Chapter 6- Loops/Practise/Practise3.py
@@ -0,0 +1,9 @@
+#variable to store the sum
+sum_numbers = 0
+
+# Using a for loop to find the sum of numbers from 1 to 100
+for number in range(1, 101):
+    sum_numbers += number
+
+# Printing the sum
+print("Sum of numbers from 1 to 100:", sum_numbers)
