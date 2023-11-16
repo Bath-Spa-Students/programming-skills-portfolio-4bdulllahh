7e63cd57c069acc61458b358674224d8caf42a11
4bdulllahh <abdullahkamran823@gmail.com>
2023-11-16 11:11:01 +0400

    Practise 1

--- Chapter 4- Control Flow/Practise/Practise1.py
+++ Chapter 4- Control Flow/Practise/Practise1.py
@@ -1,10 +1,15 @@
 x = float(input("Enter a value for x from 10 to 190: "))
-#take input from user
+# take input from user
+
+# Initializing y and z with default values
+y = 0
+z = 0
+
 # Using an if statement to check if x is greater than 100
 if x > 100:
     y = 20
     z = 40
-    #diffrent messages for diffrent scenarios
+    # different messages for different scenarios
     print(f"x is greater than 100. The Value of y is changed into 20, and the value of z is assigned to 40.")
 else:
     print(f"x is not greater than 100. The value of y and z does not change.")
