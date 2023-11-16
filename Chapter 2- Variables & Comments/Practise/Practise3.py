94ceba99347e0dd5002e8834c97b51742782f1b5
4bdulllahh <abdullahkamran823@gmail.com>
2023-11-16 10:47:20 +0400

    Practise 3

--- Chapter 2- Variables & Comments/Practise/Practise3.py
+++ Chapter 2- Variables & Comments/Practise/Practise3.py
@@ -1,16 +1,17 @@
 # Take input from the user five times
-input = []
+user_input = []
 for i in range(5):
     value = input(f"Enter value {i + 1}: ")
-    input.append(value)
+    user_input.append(value)
 
 # Typecast to string, int, and float
-string_variable = str(input[0])
-int_variable = int(input[1])
-float_variable = float(input[2])
+string_variable = str(user_input[0])
+int_variable = int(user_input[1])
+float_variable = float(user_input[2])
 
 # Print the variables
 print("\nVariables:")
 print(f"String Variable: {string_variable}")
 print(f"Integer Variable: {int_variable}")
 print(f"Float Variable: {float_variable}")
+
